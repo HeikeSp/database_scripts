ae10237181f78b25f96273f99f57ecc574598d9f
Heike Sprenger <HeikeSp@yahoo.com>
2015-06-15 16:20:58 +0200

    small change

--- rwc_query-2014-06-24.sql
+++ rwc_query-2014-06-24.sql
@@ -31,5 +31,12 @@ P3.number AS SW,
      LEFT JOIN aliquot_samples AS ASA ON ASA.aliquot_id = A.id
      WHERE V.id IN (169,170,171,172)
 ) AS Base
-LEFT JOIN subspecies AS SUB ON SUB.id = Base.subspecies_id LEFT JOIN sample_times ST on ST.culture_id = Base.experiment_id and ST.sample_date = Base.sample_date LEFT JOIN phenotype_plants PP1 ON PP1.plant_id = Base.plant_id LEFT JOIN phenotypes P1 ON P1.id = PP1.phenotype_id AND Base.sample_date = P1.date AND P1.value_id = 55 AND P1.entity_id = 366 LEFT JOIN phenotype_plants PP2 ON PP2.plant_id = Base.plant_id LEFT JOIN phenotypes P2 ON P2.id = PP2.phenotype_id AND P2.value_id = 69 AND P2.entity_id = 366 AND ((P2.date <= (P1.date + INTERVAL 7 DAY)) AND (P2.date >= (P1.date + INTERVAL 1 DAY))) LEFT JOIN phenotype_plants PP3 ON PP3.plant_id = Base.plant_id LEFT JOIN phenotypes P3 ON P3.id = PP3.phenotype_id AND P3.value_id =
-156 AND P3.entity_id = 366 AND ((P3.date <= (P1.date + INTERVAL 5 DAY)) AND (P3.date >= (P1.date + INTERVAL 1 DAY))) WHERE P1.number IS NOT NULL AND P1.invalid IS NULL AND P2.number IS NOT NULL AND P2.invalid IS NULL AND P3.number IS NOT NULL AND P3.invalid IS NULL
+LEFT JOIN subspecies AS SUB ON SUB.id = Base.subspecies_id 
+LEFT JOIN sample_times ST on ST.culture_id = Base.experiment_id and ST.sample_date = Base.sample_date 
+LEFT JOIN phenotype_plants PP1 ON PP1.plant_id = Base.plant_id 
+LEFT JOIN phenotypes P1 ON P1.id = PP1.phenotype_id AND Base.sample_date = P1.date AND P1.value_id = 55 AND P1.entity_id = 366 
+LEFT JOIN phenotype_plants PP2 ON PP2.plant_id = Base.plant_id 
+LEFT JOIN phenotypes P2 ON P2.id = PP2.phenotype_id AND P2.value_id = 69 AND P2.entity_id = 366 AND ((P2.date <= (P1.date + INTERVAL 7 DAY)) AND (P2.date >= (P1.date + INTERVAL 1 DAY))) 
+LEFT JOIN phenotype_plants PP3 ON PP3.plant_id = Base.plant_id 
+LEFT JOIN phenotypes P3 ON P3.id = PP3.phenotype_id AND P3.value_id = 156 AND P3.entity_id = 366 AND ((P3.date <= (P1.date + INTERVAL 5 DAY)) AND (P3.date >= (P1.date + INTERVAL 1 DAY))) 
+WHERE P1.number IS NOT NULL AND P1.invalid IS NULL AND P2.number IS NOT NULL AND P2.invalid IS NULL AND P3.number IS NOT NULL AND P3.invalid IS NULL
