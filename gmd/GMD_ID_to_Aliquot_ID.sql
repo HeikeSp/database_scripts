1dd1a37093fad00fe88a8c2fbcb14259af9a3ee2
Heike Sprenger <HeikeSp@yahoo.com>
2016-02-23 17:07:07 +0100

    small changes

--- gmd/GMD_ID_to_Aliquot_ID.sql
+++ gmd/GMD_ID_to_Aliquot_ID.sql
@@ -3,8 +3,6 @@ select * from tf.SampleInfo where tf.SampleInfo.attribute = 'aliquot id'
 select * from tf.SampleInfo where tf.SampleInfo.attribute = 'Aliquot_ID'
 select * from tf.SampleInfo where tf.SampleInfo.attribute like '%standard%'
 
-
-
 -- get all chromatograms with comment mentioning valdis and join to all samples that have aliquotID
 SELECT        GC_Chromatogram.id, GC_Chromatogram.name, GC_Chromatogram.EvaluationComment, GC_Chromatogram.Comment, 
                          GC_Chromatogram.FK_GC_ChromatogramType, tf.SampleInfo.FK_Sample, tf.SampleInfo.attribute, GC_Chromatogram.obsolete_FK_Sample, Vial.id AS Expr1, 
@@ -24,6 +22,14 @@ FROM            GC_Chromatogram INNER JOIN
                          Sample ON Vial.FK_sample = Sample.id AND Vial.FK_sample = Sample.id INNER JOIN
                          tf.SampleInfo ON Sample.id = tf.SampleInfo.FK_Sample AND Sample.id = tf.SampleInfo.FK_Sample
 WHERE        (GC_Chromatogram.Comment LIKE '%valdis%' OR GC_Chromatogram.Comment LIKE  '%trost2%') AND (tf.SampleInfo.attribute = 'Aliquot_ID')
+
+-- similar query only with biological samples
+SELECT        GC_Chromatogram.name, tf.SampleInfo.value as aliquot_id
+FROM            GC_Chromatogram INNER JOIN
+                         Vial ON GC_Chromatogram.id = Vial.FK_chromatogram INNER JOIN
+                         Sample ON Vial.FK_sample = Sample.id AND Vial.FK_sample = Sample.id INNER JOIN
+                         tf.SampleInfo ON Sample.id = tf.SampleInfo.FK_Sample AND Sample.id = tf.SampleInfo.FK_Sample
+WHERE        (GC_Chromatogram.Comment LIKE '%valdis%' OR GC_Chromatogram.Comment LIKE  '%trost2%') AND (tf.SampleInfo.attribute = 'Aliquot_ID') AND tf.SampleInfo.value != 'NA'
 
 
 -- missing data for:
