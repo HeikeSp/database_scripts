b28c9b7dc8cd0e4737d36551da620603a863a3e8
Heike Sprenger <HeikeSp@yahoo.com>
2016-02-24 17:36:09 +0100

    small changes

--- fill_aliquot_query_table.sh
+++ fill_aliquot_query_table.sh
@@ -6,5 +6,5 @@
 # matches 5 digits, 2 letters, underscore, 1 or 2 digits --> chromatogram_name (14047if_9) 
 
 echo "BEGIN;"
-cat $1 | sed "s/\([0-9]\{7\}\),\([0-9]\{5\}[a-z]\{2\}_[0-9]\{1,2\}\)/INSERT INTO aliquot_query VALUES (\2, \1, NULL, NULL);/g"
+cat $1 | sed "s/\([0-9]\{7\}\),\([0-9]\{5\}[a-z]\{2\}_[0-9]\{1,2\}\)/INSERT INTO aliquot_query VALUES ('\2', '\1', NULL, NULL);/g"
 echo "COMMIT;"
