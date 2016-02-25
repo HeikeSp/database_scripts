98cb5a5a11dabcddd44b61704e625274817a818e
Heike Sprenger <HeikeSp@yahoo.com>
2016-02-25 18:17:26 +0100

    added more queries

--- gmd/aliquot_query_input_valdis.sql
+++ gmd/aliquot_query_input_valdis.sql
@@ -7,3 +7,30 @@ FROM            GC_Chromatogram INNER JOIN
                          tf.SampleInfo ON Sample.id = tf.SampleInfo.FK_Sample AND Sample.id = tf.SampleInfo.FK_Sample
 WHERE        (GC_Chromatogram.Comment LIKE '%valdis%' OR GC_Chromatogram.Comment LIKE  '%trost2%') AND (tf.SampleInfo.attribute = 'Aliquot_ID') AND tf.SampleInfo.value != 'NA'
 ORDER BY tf.SampleInfo.value
+
+
+-- # mpi test 1.2 AD45E299-4199-4341-92E7-FB71724FD471
+SELECT DISTINCT       GC_Chromatogram.name, tf.SampleInfo.attribute, tf.SampleInfo.value
+FROM            GC_Chromatogram INNER JOIN
+                         Vial ON GC_Chromatogram.id = Vial.FK_chromatogram INNER JOIN
+                         Sample ON Vial.FK_sample = Sample.id AND Vial.FK_sample = Sample.id INNER JOIN
+                         tf.SampleInfo ON Sample.id = tf.SampleInfo.FK_Sample AND Sample.id = tf.SampleInfo.FK_Sample
+						 INNER JOIN tf.IntensityValue ON GC_Chromatogram.id = tf.IntensityValue.FK_chromatogram
+WHERE        tf.IntensityValue.FK_TagList = 'AD45E299-4199-4341-92E7-FB71724FD471' and GC_Chromatogram.name = '11290if_52'
+
+SELECT DISTINCT       GC_Chromatogram.name, tf.SampleInfo.attribute, tf.SampleInfo.value, tf.IntensityValue.FK_TagList
+FROM            GC_Chromatogram INNER JOIN
+                         Vial ON GC_Chromatogram.id = Vial.FK_chromatogram INNER JOIN
+                         Sample ON Vial.FK_sample = Sample.id AND Vial.FK_sample = Sample.id INNER JOIN
+                         tf.SampleInfo ON Sample.id = tf.SampleInfo.FK_Sample AND Sample.id = tf.SampleInfo.FK_Sample
+						 INNER JOIN tf.IntensityValue ON GC_Chromatogram.id = tf.IntensityValue.FK_chromatogram
+WHERE        GC_Chromatogram.name = '14029f_77'
+
+
+SELECT DISTINCT       GC_Chromatogram.name, tf.SampleInfo.attribute, tf.SampleInfo.value
+FROM            GC_Chromatogram INNER JOIN
+                         Vial ON GC_Chromatogram.id = Vial.FK_chromatogram INNER JOIN
+                         Sample ON Vial.FK_sample = Sample.id AND Vial.FK_sample = Sample.id INNER JOIN
+                         tf.SampleInfo ON Sample.id = tf.SampleInfo.FK_Sample AND Sample.id = tf.SampleInfo.FK_Sample
+						 INNER JOIN tf.IntensityValue ON GC_Chromatogram.id = tf.IntensityValue.FK_chromatogram
+WHERE        tf.IntensityValue.FK_TagList = 'AD45E299-4199-4341-92E7-FB71724FD471' and tf.SampleInfo.attribute = 'TROST: SAMPTEXT / AliquotID'
