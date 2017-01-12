89d4d55184f67f5b3948dcf23574ecb45fcb73a8
Heike Sprenger <HeikeSp@yahoo.com>
2017-01-12 13:56:48 +0100

    small changes

--- update_plantlines_alias.sql
+++ update_plantlines_alias.sql
@@ -1,6 +1,8 @@
 select * from plantlines;
 
-update plantlines set line_alias='Desiree' where id=22497;
-update plantlines set line_alias='Albatros' where id=866296;
-update plantlines set line_alias='Ramses' where id=858641;
-update plantlines set line_alias='Euroresa' where id=869004;
+update plantlines set line_alias = 'Desiree' where id IN(7864,22497,856447,859965,878456,899419,920074,932966,946605);
+update plantlines set line_alias = 'Albatros' where id IN(856448,860199,866296,875395,897818,917831,947269); 
+update plantlines set line_alias = 'Ramses' where id IN(856459,858641,860205,877819,897363,918738,947273);
+update plantlines set line_alias = 'Euroresa' where id IN(856434,860211,869004,875436,897270,917260,947272);
+
+
